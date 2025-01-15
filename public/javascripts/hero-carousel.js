7f4c154c4a0d353a279c320e2eb87b2dee4ed5e2
Tim El Moughayar <tim.elmoughayar@icloud.com>
2025-01-14 21:20:56 -0600

    setup html and css for hero carousel. Initialized 404 not found page.

---
+++ public/javascripts/hero-carousel.js
@@ -0,0 +1,11 @@
+const track = document.querySelector('.carousel-track');
+const slides = Array.from(track.children);
+
+console.log(track);
+
+
+
+
+// when I click left, move slides to the left
+// when I click right, move slides to the right
+// when I click nav indicator, move to that slide
