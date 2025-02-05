bc7ee2dcde9546c9f632081238c1eab026063180
Tim El Moughayar <tim.elmoughayar@icloud.com>
2025-02-04 22:43:11 -0600

    Created working carousel. changed homepage to BEM name convention.

--- README.md
+++ README.md
@@ -4,4 +4,4 @@
 
 ## Website: https://jac-wraps-demo.onrender.com <br>
 ## Design: [Figma - Jac Wraps](https://www.figma.com/proto/DvQwA12ntlqbYsfeAAhsHp/Jac-Wraps?node-id=209-247&node-type=canvas&t=iS1JfqM85DyU65Qj-0&scaling=min-zoom&content-scaling=fixed&page-id=0%3A1&starting-point-node-id=209%3A247&share=1&hide-ui=1)
-## Original live site (not my design): https://jacwrap
+## Original live site (not my design): https://jacwraps.com
--- public/javascripts/hero-carousel.js
+++ public/javascripts/hero-carousel.js
@@ -1,47 +1,64 @@
-const track = document.querySelector('.hero-track');
-const trackContainer = document.querySelector('.hero-track-container');
+const track = document.querySelector('.hero__track');
+const trackContainer = document.querySelector('.hero__track-container');
 const slides = Array.from(track.children);
 const nextButton = document.querySelector('.hero__arrow--right');
 const prevButton = document.querySelector('.hero__arrow--left');
-const dotsNav = document.querySelector('.slide_nav');
+const dotsNav = document.querySelector('.hero__dot-indicator-container');
 const dots = Array.from(dotsNav.children);
 
 const slideWidth = slides[0].getBoundingClientRect().width;
-console.log('slides', slides);
-console.log('slideWidth', slideWidth);
-
+trackContainer.style.maxWidth = slideWidth + 'px';
 
 const setSlidePosition = (slide, index) => {
     slide.style.left = slideWidth * index + 'px';
 }
 slides.forEach(setSlidePosition);
 
-console.log();
-console.log(track);
-
 const moveToSlide = (track, currentSlide, targetSlide) => { 
     track.style.transform = 'translateX(-' + targetSlide.style.left + ')';
     console.log('targetSlide.style.left', targetSlide.style.left);
     console.log('slideWidth', slideWidth);
-    trackContainer.style.maxWidth = slideWidth + 'px';
     currentSlide.classList.remove('current-slide');
     targetSlide.classList.add('current-slide');
 }
 
-// when I click left, move slides to the left
+const updateDots = (currentDot, targetDot) => {
+    currentDot.classList.remove('current-slide');
+    targetDot.classList.add('current-slide');
+}
+
 prevButton.addEventListener('click', e => {
     const currentSlide = track.querySelector('.current-slide')
     const preSlide = currentSlide.previousElementSibling;
+    const currentDot = dotsNav.querySelector('.current-slide');
+    const prevDot = currentDot.previousElementSibling;
 
     moveToSlide(track, currentSlide, preSlide);
+    updateDots(currentDot, prevDot);
 })
 
-// when I click right, move slides to the right
 nextButton.addEventListener('click', e => {
     const currentSlide = track.querySelector('.current-slide');
-    const nextSlide = currentSlide.nextElementSibling;    
+    const nextSlide = currentSlide.nextElementSibling;
+    const currentDot = dotsNav.querySelector('.current-slide');
+    const nextDot = currentDot.nextElementSibling;
 
     moveToSlide(track, currentSlide, nextSlide);
+    updateDots(currentDot, nextDot);
 }) 
 
 // when I click nav indicator, move to that slide
+
+dotsNav.addEventListener('click', e => {
+    const targetDot = e.target.closest('.hero__dot-indicator');
+
+    if (!targetDot) return;
+
+    const currentSlide = track.querySelector('.current-slide');
+    const currentDot = dotsNav.querySelector('.current-slide');
+    const targetIndex = dots.findIndex(dot => dot === targetDot);
+    const targetSlide = slides[targetIndex];
+
+    moveToSlide(track, currentSlide, targetSlide);
+    updateDots(currentDot, targetDot);
+});
