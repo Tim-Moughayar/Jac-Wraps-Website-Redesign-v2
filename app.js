8654889a2f3ebd53ce024bc6a4cde3f7254de4ca
Tim El Moughayar <tim.elmoughayar@icloud.com>
2025-02-28 15:10:59 -0600

    added descriptions to meta tags and alt tags for pause button

--- app.js
+++ app.js
@@ -14,27 +14,27 @@ app.set('views', path.join(__dirname, 'views'))
 app.use(express.static(path.join(__dirname, 'public')))
 
 app.get('/', (req, res) => {
-    res.render('home', {title: 'Home'})
+    res.render('home', {title: 'Home', description: "Enhance your brand visibility with fleet and storefront graphics in Chicago."})
 })
 
 app.get('/services', (req, res) => {
-    res.render('services/index', {title: 'Services'});
+    res.render('services/index', {title: 'Services', description: "We offer vinyl solutions for vehicles, walls, and windows. Inside and out. "});
 })
 
 app.get('/contact', (req, res) => {
-    res.render('contact/index', {title: 'Contact'});
+    res.render('contact/index', {title: 'Contact', description: "Contact us today for a free estimate!"});
 })
 
 app.get('/about', (req, res) => {
-    res.render('about/index', {title: 'About'});
+    res.render('about/index', {title: 'About', description: "Jac Wraps is a family owned business, proudly serving nationwide clients and our local communities."});
 })
 
 app.get('/services/vehicle-wraps', (req, res) => {
-    res.render('services/vehicle-wraps/index', {title: 'Vehicle Wraps'});
+    res.render('services/vehicle-wraps/index', {title: 'Vehicle Wraps', description: "We provide fleet graphics, color changes, chrome deletion, and paint protection film."});
 })
 
 app.get('/services/architectural-graphics', (req, res) => {
-    res.render('services/architectural-graphics/index', {title: 'Architectural Graphics'});
+    res.render('services/architectural-graphics/index', {title: 'Architectural Graphics', description: "We provide fleet graphics, color changes, chrome deletion, and paint protection film."});
 })
 
 
